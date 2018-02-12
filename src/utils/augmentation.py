c9f6526841edcd791ae92dd0c128a78c1a424313
Jeff Wen <jeff.li.wen@gmail.com>
2018-02-11 20:43:58 -0500

    restructure file locations and update mask tensor

--- src/utils/augmentation.py
+++ src/utils/augmentation.py
@@ -1,67 +1,8 @@
-from __future__ import print_function, division
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-from skimage import io, transform
-from skimage.color import gray2rgb
+from skimage import transform
+from torchvision import transforms
 
-import os
-import torch
-import pandas as pd
 import numpy as np
-import random
-import matplotlib.pyplot as plt
-
-# ignore warnings
-import warnings
-
-warnings.filterwarnings("ignore")
-
-# interactive mode
-plt.ion()
-
-local_path = '/Users/jwen/Projects/road_building_extraction/'
-
-
-class MassRoadBuildingDataset(Dataset):
-    """Massachusetts Road and Building dataset"""
-
-    def __init__(self, csv_file, root_dir='mass_roads', train_valid_test='train', transform=None):
-        """
-        Args:
-            csv_file (string): Path to the csv file with image paths
-            train_valid_test (string): 'train', 'valid', or 'test'
-            root_dir (string): 'mass_roads' or 'mass_buildings'
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
-        self.train_valid_test = train_valid_test
-        self.root_dir = root_dir
-        self.img_path_df = self._filter_df(csv_file)
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.img_path_df)
-
-    def __getitem__(self, idx):
-        sat_img_name = os.path.join('data', self.root_dir, self.train_valid_test, 'sat',
-                                    self.img_path_df.loc[idx, 'sat_img_path'])
-        sat_image = io.imread(sat_img_name)
-
-        map_img_name = os.path.join('data', self.root_dir, self.train_valid_test, 'map',
-                                    self.img_path_df.loc[idx, 'map_img_path'])
-        map_image = io.imread(map_img_name)
-
-        sample = {'sat_img': sat_image, 'map_img': map_image}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
-
-    def _filter_df(self, csv_file):
-        df = pd.read_csv(csv_file)
-
-        return df[(df['train_valid_test'] == self.train_valid_test) &
-                  (df['sat_map'] != 'missing')].reset_index(drop=True)
+import torch
 
 
 class RescaleTarget(object):
@@ -176,8 +117,9 @@ class ToTensorTarget(object):
         # numpy image: H x W x C
         # torch image: C X H X W
         sat_img = sat_img.transpose((2, 0, 1))
+
         return {'sat_img': torch.from_numpy(sat_img),
-                'map_img': torch.from_numpy(map_img)}
+                'map_img': torch.from_numpy(map_img).unsqueeze(0)} # unsqueeze for the channel dimension
 
 
 class NormalizeTarget(transforms.Normalize):
@@ -186,43 +128,3 @@ class NormalizeTarget(transforms.Normalize):
     def __call__(self, sample):
         return {'sat_img': transforms.functional.normalize(sample['sat_img'], self.mean, self.std),
                 'map_img': sample['map_img']}
-
-
-# helper function for viewing images
-def show_map(sat_img, map_img=None, sat_img_path=None, axis=None):
-    """
-    Return an image with the shape mask if there is one supplied
-    """
-
-    if axis:
-        axis.imshow(sat_img)
-
-        if map_img is not None:
-            axis.imshow(map_img, alpha=0.5, cmap='gray')
-
-    else:
-        plt.imshow(sat_img)
-
-        if map_img is not None:
-            plt.imshow(map_img, alpha=0.5, cmap='gray')
-
-
-# helper function to show a batch
-def show_map_batch(sample_batched):
-    """Show image with landmarks for a batch of samples."""
-    sat_img_batch, map_img_batch = \
-        sample_batched['sat_img'], sample_batched['map_img']
-    batch_size = len(sat_img_batch)
-    img_size = sat_img_batch.size(2)
-
-    f, ax = plt.subplots(int(np.ceil(batch_size / 3)), 3, figsize=(15, int(np.ceil(batch_size / 3)) * 5))
-    f.tight_layout()
-    f.subplots_adjust(hspace=.05, wspace=.05)
-    ax = ax.ravel()
-
-    for i in range(batch_size):
-        print(type(ax[i]))
-        ax[i].axis('off')
-        show_map(sat_img=sat_img_batch.numpy()[i, :, :, :].transpose((1, 2, 0)),
-                 map_img=map_img_batch.numpy()[i, :, :], axis=ax[i])
-
---
+++ src/utils/data_utils.py
@@ -0,0 +1,97 @@
+from __future__ import print_function, division
+from torch.utils.data import Dataset
+from torchvision import transforms, utils
+from skimage import io
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+
+warnings.filterwarnings("ignore") # ignore warnings
+
+plt.ion() # interactive mode
+
+local_path = '/Users/jwen/Projects/road_building_extraction/'
+
+
+class MassRoadBuildingDataset(Dataset):
+    """Massachusetts Road and Building dataset"""
+
+    def __init__(self, csv_file, root_dir='mass_roads', train_valid_test='train', transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with image paths
+            train_valid_test (string): 'train', 'valid', or 'test'
+            root_dir (string): 'mass_roads' or 'mass_buildings'
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.train_valid_test = train_valid_test
+        self.root_dir = root_dir
+        self.img_path_df = self._filter_df(csv_file)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_path_df)
+
+    def __getitem__(self, idx):
+        sat_img_name = os.path.join('data', self.root_dir, self.train_valid_test, 'sat',
+                                    self.img_path_df.loc[idx, 'sat_img_path'])
+        sat_image = io.imread(sat_img_name)
+
+        map_img_name = os.path.join('data', self.root_dir, self.train_valid_test, 'map',
+                                    self.img_path_df.loc[idx, 'map_img_path'])
+        map_image = io.imread(map_img_name)
+
+        sample = {'sat_img': sat_image, 'map_img': map_image}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def _filter_df(self, csv_file):
+        df = pd.read_csv(csv_file)
+
+        return df[(df['train_valid_test'] == self.train_valid_test) &
+                  (df['sat_map'] != 'missing')].reset_index(drop=True)
+
+
+# helper function for viewing images
+def show_map(sat_img, map_img=None, axis=None):
+    """
+    Return an image with the shape mask if there is one supplied
+    """
+
+    if axis:
+        axis.imshow(sat_img)
+
+        if map_img is not None:
+            axis.imshow(map_img, alpha=0.5, cmap='gray')
+
+    else:
+        plt.imshow(sat_img)
+
+        if map_img is not None:
+            plt.imshow(map_img, alpha=0.5, cmap='gray')
+
+
+# helper function to show a batch
+def show_map_batch(sample_batched):
+    """Show image with landmarks for a batch of samples."""
+    sat_img_batch, map_img_batch = \
+        sample_batched['sat_img'], sample_batched['map_img']
+    batch_size = len(sat_img_batch)
+
+    f, ax = plt.subplots(int(np.ceil(batch_size / 3)), 3, figsize=(15, int(np.ceil(batch_size / 3)) * 5))
+    f.tight_layout()
+    f.subplots_adjust(hspace=.05, wspace=.05)
+    ax = ax.ravel()
+
+    for i in range(batch_size):
+        print(type(ax[i]))
+        ax[i].axis('off')
+        show_map(sat_img=sat_img_batch.numpy()[i, :, :, :].transpose((1, 2, 0)),
+                 map_img=map_img_batch.numpy()[i, 0, :, :], axis=ax[i])
